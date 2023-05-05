1155a3c778086fc357c18a190c59765e0a434f6b
Giovanni MION <giovanni.mion@forvia.com>
2023-05-05 19:13:22 +0200

    Update Nvim config

---
+++ config/nvim/lua/custom/custom/configs/lspconfig.lua
@@ -0,0 +1,18 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "als", "bashls", "clangd", "cmake", "groovyls", "kotlin_language_server", "pylsp", "pylyzer", "vimls", "yamlls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+      virtual_text = false
+    }
+)
