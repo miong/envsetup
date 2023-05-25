7bbe835057c5b01d369b5def1afffd7da76443ee
Giovanni MION <giovanni.mion@forvia.com>
2023-05-25 11:32:54 +0200

    Update config

--- config/nvim/lua/custom/chadrc.lua
+++ config/nvim/lua/custom/chadrc.lua
@@ -1,3 +1,12 @@
+local opt = vim.opt
+-------------------------------------- options ------------------------------------------
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+
 ---@type ChadrcConfig 
 local M = {}
 M.ui = {theme = 'catppuccin'}
